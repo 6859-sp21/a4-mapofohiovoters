7ed9408c59675aaf9076c93777adeccdff42052b
Rishi Shah <rshah36@mit.edu>
2021-04-06 22:30:26 -0400

    slider work

--- map.js
+++ map.js
@@ -3,7 +3,10 @@ async function createMap() {
     const width = 1000;
     const height = 800;
 
-    const projection = d3.geoEquirectangular().fitExtent([[0,0], [width, height]], ohioCounties);
+    const startDate = new Date("2016-11-03"),
+        endDate = new Date("2020-10-06");
+
+    const projection = d3.geoEquirectangular().fitExtent([[0, 0], [width, height]], ohioCounties);
     const path = d3.geoPath().projection(projection);
 
     var isZoomed = false;
@@ -14,29 +17,99 @@ async function createMap() {
 
     const svg = d3.select("#visualization-container")
         .append("svg")
-            .attr('width', width)
-            .attr('height', height);
+        .attr('width', width)
+        .attr('height', height);
 
-    const g = svg.append('g')
+    ///
+
+    var formatDateIntoYear = d3.timeFormat("%Y");
+    var formatDate = d3.timeFormat("%b %Y");
+
+    // var margin = {top: 0, right: 50, bottom: 0, left: 50},
+
+    var x = d3.scaleTime()
+        .domain([startDate, endDate])
+        .range([0, width])
+        .clamp(true);
+
+    var slider = svg.append("g")
+        .attr("class", "slider")
+        // .attr("transform", "translate(" + margin.left + "," + height / 2 + ")");
+
+    slider.append("line")
+        .attr("class", "track")
+        .attr("x1", x.range()[0])
+        .attr("x2", x.range()[1])
+        .select(function () {
+            return this.parentNode.appendChild(this.cloneNode(true));
+        })
+        .attr("class", "track-inset")
+        .select(function () {
+            return this.parentNode.appendChild(this.cloneNode(true));
+        })
+        .attr("class", "track-overlay")
+        .call(d3.drag()
+            .on("start.interrupt", function () {
+                slider.interrupt();
+            })
+            .on("start drag", function () {
+                // update date/opacity
+                hue(x.invert(d3.event.x));
+            }));
+
+    slider.insert("g", ".track-overlay")
+        .attr("class", "ticks")
+        .attr("transform", "translate(0," + 18 + ")")
+        .selectAll("text")
+        .data(x.ticks(10))
+        .enter()
+        .append("text")
+        .attr("x", x)
+        .attr("y", 10)
+        .attr("text-anchor", "middle")
+        .text(function (d) {
+            return formatDateIntoYear(d);
+        });
+
+    var label = slider.append("text")
+        .attr("class", "label")
+        .attr("text-anchor", "middle")
+        .text(formatDate(startDate))
+        .attr("transform", "translate(0," + (-25) + ")")
+
+    var handle = slider.insert("circle", ".track-overlay")
+        .attr("class", "handle")
+        .attr("r", 9);
+
+    function hue(h) {
+        handle.attr("cx", x(h));
+        label
+            .attr("x", x(h))
+            .text(formatDate(h));
+        svg.style("background-color", d3.hsl(h / 1000000000, 0.8, 0.8));
+    }
+
+
+    const ohio = svg.append('g')
         .attr('width', '100%')
         .attr('height', '100%');
 
-    const counties = g.append('g')
-            .attr('stroke', '#f5f6f7')
-            .attr('stroke-width', 1)
-            .attr('fill', '#444')
-            .attr('cursor', 'pointer')
-            .attr('width', '100%')
-            .attr('height', '100%')
+    const counties = ohio.append('g')
+        .attr('stroke', '#f5f6f7')
+        .attr('stroke-width', 1)
+        .attr('fill', '#444')
+        .attr('cursor', 'pointer')
+        .attr('width', '100%')
+        .attr('height', '100%')
         .selectAll('path')
         .data(ohioCounties.features)
         .join('path')
-            .on('click', function(e, d) {
-                isZoomed ? reset() : clicked(e, d, this)
-            })
-            .on('mouseover', hoveringStart)
-            .on('mouseout', hoveringEnd)
-            .attr('d', path);
+        .on('click', function (e, d) {
+            isZoomed ? reset() : clicked(e, d, this)
+        })
+        .on('mouseover', hoveringStart)
+        .on('mouseout', hoveringEnd)
+        .attr('d', path);
 
     counties.append("title")
         .text(d => d.properties.name);
